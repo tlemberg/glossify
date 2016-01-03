7c16166aa823a89a053f450f1a6740d20b1191af
Thomas Lemberg <tlemberg@Thomass-MacBook-Pro.local>
2016-01-03 13:42:28 -0800

    rate limit

--- web/pylib/translations.py
+++ web/pylib/translations.py
@@ -1,10 +1,45 @@
 import json, os, requests
 from multiprocessing.dummy import Pool as ThreadPool
+import time
+import threading
+
+from functools import wraps
 
 API_BASE_URL = 'https://www.googleapis.com/language/translate/v2'
 API_KEY = os.environ['GOOGLE_API_KEY']
 
 
+def rate_limited(max_per_second):
+    """
+    Decorator that make functions not be called faster than
+    """
+    lock = threading.Lock()
+    min_interval = 1.0 / float(max_per_second)
+
+    def decorate(func):
+        last_time_called = [0.0]
+
+        @wraps(func)
+        def rate_limited_function(*args, **kwargs):
+            lock.acquire()
+            elapsed = time.clock() - last_time_called[0]
+            left_to_wait = min_interval - elapsed
+
+            if left_to_wait > 0:
+                time.sleep(left_to_wait)
+
+            lock.release()
+
+            ret = func(*args, **kwargs)
+            last_time_called[0] = time.clock()
+            return ret
+
+        return rate_limited_function
+
+    return decorate
+
+
+@rate_limited(99)
 def translate(q, source, target):
 	payload = {
 		'q': q,
@@ -26,7 +61,7 @@ def translate(q, source, target):
 	
 
 
-def pooled_translate(qs, source, target, n_pools=3):
+def pooled_translate(qs, source, target, n_pools=5):
 	pool = ThreadPool(n_pools)
 	results = pool.map(lambda q: (q, translate(q, source, target)), qs)
 	pool.close()
